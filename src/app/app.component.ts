0ce4404694c305b1c0d0a1cade9235f7eb6a214e
zazk <juandedioz@gmail.com>
2017-09-26 12:07:48 -0500

    Added Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ],
+	encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
 }
