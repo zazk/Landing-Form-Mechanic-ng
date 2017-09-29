eb9474eb79f0c8e61d919fe92fa7cea8b57d8ded
zazk <juandedioz@gmail.com>
2017-09-28 23:29:46 -0500

    Add Insert to API. Update Component Name

--- api/api.landing.php
+++ api/api.landing.php
@@ -1,10 +1,47 @@
 <?php
 // process.php
 header('Access-Control-Allow-Origin: *');
+
+// LESSQL
+
+// Connection
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once("./LessQL/Database.php");
+require_once("./LessQL/Result.php");
+require_once("./LessQL/Row.php");
+
+
+$dsn = 'mysql:host=localhost;dbname=brushlee_mecanico';
+$username = 'brushlee_mecanic';
+$password = 'mecanico';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+
+$pdo = new PDO($dsn, $username, $password, $options);
+
+$db = new LessQL\Database( $pdo );
+$result = $db->mecanicos();
+
+$statement = $result->insert( array(
+  'nombres' => 'Prueba Nombres 1',
+  'apellidopaterno' => 'Prueba Nombres 1',
+  'apellidomaterno' => 'Prueba Nombres 1',
+  'celular' => 'Prueba Nombres 1',
+  'email' => 'Prueba Nombres 1',
+  'fechanacimiento' => '2010-10-10',
+  'telefonofijo' => 'Prueba Nombres 1',
+  'distritovives' => 'Prueba Nombres 1',
+  'educacion' => 'Prueba Nombres 1',
+  ) );
+
+print_r($db->mecanicos()->fetchAll() );
+
+// Manage Data
 $errors = array();  // array to hold validation errors
 $data = array();        // array to pass back data
-
-// validate the variables ========  
+// validate the variables ========
 if (empty($_POST['name']))
   $errors['name'] = 'Name is required.';
 
