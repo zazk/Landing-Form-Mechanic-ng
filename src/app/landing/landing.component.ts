d7fd614ed7ab18eb27b2c36b6ac3486c7a16fbae
zazk <juandedioz@gmail.com>
2017-09-29 13:50:41 -0500

    Update Validate, add send message for Success Form Sent

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,28 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
 	constructor(private http: HttpClient) {}
 
-	onSubmitForm(event) {
+	onSubmitForm(form) {
+		const event = form.value;
 		console.log('submit Form now', event);
-
+		console.log('Form Valid?', form.valid);
 		// Replace data to ecode for PHP way. Note: PHP sucks!
 		const data = JSON.stringify(event);
 		const params = 'data=' + data;
-
-		this.http
-			.post('http://w.areminds.com/api/index.php', params, {
-				headers: new HttpHeaders().set(
-					'Content-Type',
-					'application/x-www-form-urlencoded'
-				)
-			})
-			.subscribe(
-				(res) => {
-					console.log('Response:', res);
-				},
-				(error) => {
-					console.log('Error:=======', error);
-				}
-			);
+		if (form.valid) {
+			this.http
+				.post('http://w.areminds.com/api/index.php', params, {
+					headers: new HttpHeaders().set(
+						'Content-Type',
+						'application/x-www-form-urlencoded'
+					)
+				})
+				.subscribe(
+					(res) => {
+						console.log('Response:', res);
+						form.reset();
+						alert(
+							'Gracias por Registrarse, Enviaremos un correo de confirmación'
+						);
+					},
+					(error) => {
+						console.log('Error:=======', error);
+					}
+				);
+		} else {
+			return alert('Por favor llenar todos los datos correctamente');
+		}
 		return false;
 	}
 
